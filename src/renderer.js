3b6f4b98e6063e2eaf70b1f97119fab1bd7bed95
Christian Burland Mankowski <chrisaburland@gmail.com>
2017-08-27 15:10:10 +0100

    Loads of work, nearly functional.

--- src/renderer.js
+++ src/renderer.js
@@ -5,41 +5,86 @@ const config = require('./config')
 /**
  * Create a torrent client.
  */
-const client = new WebTorrent(config.wt.client)
+const client = new WebTorrent(config.webtorrent.client)
 
+/**
+ * Main component, shown on app load.
+ */
 class Main extends Component {
   constructor() {
     super()
+    this.state = {
+      link: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  onTorrent(link) {
-    this.props.onTorrent(link)
+  handleChange(e) {
+    this.setState({ link: e.target.value })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.props.onSubmit(this.state.link)
   }
 
   render() {
-    return h('div', { id: 'drop_box' },
-      h('div', { id: 'border' },
-        h('span', { id: 'drop_text' }, 'Drop Magnet or .torrent')
-      )
+    return h('form', { onSubmit: this.handleSubmit},
+      h('input', { id: "link", value: this.state.link, onChange: this.handleChange, autoFocus: true })
     )
   }
 }
 
 /**
- * Screen component.
+ * Screen component, shows video stream.
  */
 class Screen extends Component {
   constructor() {
     super()
+    this.state = {
+      ready: false,
+      url: null
+    }
+    this.server = null
+    this.torrent = null
+    this.startStream()
+  }
+
+  startStream() {
+    this.torrent = client.add(this.props.stream)
+    this.torrent.once('ready', () => {
+      this.server = torrent.createServer()
+      this.server.listen(0, () => {
+        const port = server.address().port
+        const url = `http://127.0.0.1:${port}/0`
+        this.setState({
+          ready: true,
+          url: url
+        })
+      })
+    })
+  }
+
+  destroyStream() {
+    this.torrent = null
+    this.server = null
   }
 
   render() {
-    return null
+    if (this.state.ready === false) {
+      return h('p', null, 'Loading...')
+    } else {
+      return h('video', { id: 'video', controls: true },
+        h('source', { src: this.state.url })
+      )
+    }
   }
 }
 
 /**
- * App component.
+ * App component, handles app state.
  */
 class App extends Component {
   constructor() {
@@ -47,9 +92,11 @@ class App extends Component {
     this.state = {
       stream: null
     }
+
+    this.onSubmit = this.onSubmit.bind(this)
   }
 
-  onTorrent(link) {
+  onSubmit(link) {
     this.setState({ stream: link })
   }
 
@@ -58,10 +105,10 @@ class App extends Component {
   }
 
   render() {
-    if (this.state.stream == null) {
-      return h(Main, { onTorrent: this.onTorrent })
+    if (this.state.stream === null) {
+      return h(Main, { onSubmit: this.onSubmit })
     } else {
-      return h(Screen, { infoHash: this.state.stream.info_hash, onBackButton: this.onBackButton })
+      return h(Screen, { stream: this.state.stream, onBackButton: this.onBackButton })
     }
   }
 }
