e1ecc022459cca4492a79f3c44a59d479953bc64
Christian Burland Mankowski <chrisaburland@gmail.com>
2017-08-28 12:30:17 +0100

    Removed some window code.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 const electron = require('electron')
-// Module to control application life.
 const app = electron.app
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
+const ipcMain = electron.ipcMain
 
 const path = require('path')
 const url = require('url')
@@ -17,6 +16,7 @@ function createWindow () {
     width: 800, 
     height: 456,
     title: 'Beam',
+    // titleBarStyle: 'hidden',
     backgroundColor: '#161616',
     backgroundThrottling: false
   })
@@ -47,20 +47,5 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit()
 })
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
