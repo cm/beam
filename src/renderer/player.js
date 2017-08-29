4e1256467fc209d1aac9b4b84539839b83286680
Christian Burland Mankowski <chrisaburland@gmail.com>
2017-08-29 11:33:18 +0100

    Changes to video control implementation

--- src/renderer/player.js
+++ src/renderer/player.js
@@ -8,59 +8,69 @@ const config = require('../config')
 const client = new WebTorrent(config.webtorrent.client)
 
 /**
- * Screen component, shows video stream.
+ * Screen component, handles torrent client, controls and video.
  */
 module.exports = class Player extends Component {
   constructor() {
     super()
     this.state = {
-      ready: false,
-      url: null
+      ready: false
     }
     this.server = null
     this.torrent = null
+    this.video = null
   }
 
-  startStream(stream) {
+  /**
+   * Load the stream after render is complete. (very hacky feeling)
+   */
+  componentDidMount() {
+    this.video = document.getElementById('video')
     if (this.state.ready !== true) {
-      this.torrent = client.add(stream)
+      this.torrent = client.add(this.props.stream)
       this.torrent.once('ready', () => {
         this.server = this.torrent.createServer()
         this.server.listen(0, () => {
           let port = this.server.address().port
           let url = `http://127.0.0.1:${port}/0`
           this.setState({
-            ready: true,
-            url: url
+            ready: true
           })
+          // Control video without changing state
+          this.video.src = url
+          this.video.load()
+          this.video.play()
         })
       })
     }
   }
 
-  destroyStream() {
+  handleBackButton() {
     this.server.destroy()
     this.torrent.destroy()
     this.server = null
     this.torrent = null
-  }
-
-  handleBackButton() {
-    this.destroyStream()
     this.props.onBackButton()
   }
 
+  handleControls() {
+    // to do
+  }
+
   render() {
-    this.startStream(this.props.stream)
-    if (this.state.ready === false) {
-      return h('p', null, 'Loading...')
-    } else {
-      return h('div', { id: 'player' },
-        h('video', { id: 'video', controls: true },
-          h('source', { src: this.state.url })
-        ),
-        h('div', { id: 'overlay' })
-      )
-    }
+    return h('div', { id: 'player' },
+      h('video', { id: 'video', controls: false }),
+      h(Controls)
+    )
+  }
+}
+
+class Controls extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return null
   }
 }
